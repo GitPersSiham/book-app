41eb3f888e458f0600601a47a5eb817cc64fa84f
GitPersSiham <lahouali.siham@outlook.fr>
2020-12-06 14:01:17 +0100

    formulaire signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'book-app';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBvi3PTx0m7dEV_r-_CPosKyFh7nwdwcCo",
+      authDomain: "book-app-f6f4f.firebaseapp.com",
+      projectId: "book-app-f6f4f",
+      storageBucket: "book-app-f6f4f.appspot.com",
+      messagingSenderId: "1090913685067",
+      appId: "1:1090913685067:web:fbe2459d66ad29b8c707ce",
+      measurementId: "G-H7HT46DTCX"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+  }
+ 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import firebase from "firebase";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,39 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  createNewUser(email: string, password: string ){
+  return new Promise(
+   (reslove, reject) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(
+      () => {
+        reslove();
+      },
+      ( error ) => {
+        reject(error);
+      }
+       );
+   }
+  );
+  }
+  signInUser(email: string, password: string){
+  return new Promise(
+    ( resolve, reject) => {
+     firebase.auth().signInWithEmailAndPassword(email, password).then(
+       () => {
+         resolve();
+       },
+       (error) => {
+         reject(error);
+        
+       }
+     );
+    }
+  );
+  }
+
+  signOutUser(){
+   firebase.auth().signOut();
+
+  }
 }
