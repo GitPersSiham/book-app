73f7e86195f25b80d1970b9ed383cf3cb513793f
GitPersSiham <lahouali.siham@outlook.fr>
2020-12-07 18:00:59 +0100

    les données sont enregistrés et récupéré correctement depuis la bdd

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../../models/book.model';
+import {  Router } from '@angular/router';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-single-book',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private booksService: BooksService,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.book = new Book('', '');
+    const id = this.route.snapshot.params['id'];
+    this.booksService.getSingleBook(+id).then(
+      (book: Book) => {
+        this.book = book;
+      }
+    );
   }
 
+  onBack() {
+    this.router.navigate(['/books']);
+  }
 }
